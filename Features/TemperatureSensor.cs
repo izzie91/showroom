813b789cae5aa607f3d1e87c64028c1611ba681d
izzie91 <isa910823@gmail.com>
2024-09-04 15:58:05 +0200

    boilerplate for event

---
+++ Features/TemperatureSensor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace showroom.Features;
+
+public class TemperatureSensor
+{
+    private double _temperature;
+    public event EventHandler<TemperatureChangeArgs> TemperatureChange;
+
+
+    
+    
+    protected virtual void OnTemperatureChange(TemperatureChangeArgs e)
+    {
+        TemperatureChange?.Invoke(this, e);
+    }
+
+}
+
+public class TemperatureChangeArgs : EventArgs
+{
+    public double Temperature { get; }
+    public DateTime Date { get; set; }
+}
